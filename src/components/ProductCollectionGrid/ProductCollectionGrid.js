0871f27373a8f78e06e979de7a46ce83922d70e5
Supriya K Hemanth <supriya1890@gmail.com>
2023-06-30 09:28:53 +0530

    Update ProductCollectionGrid.js

--- src/components/ProductCollectionGrid/ProductCollectionGrid.js
+++ src/components/ProductCollectionGrid/ProductCollectionGrid.js
@@ -8,25 +8,25 @@ const ProductCollectionGrid = (props) => {
     <div className={styles.root}>
       <ProductCollection
         image={'/collections/collection1.png'}
-        title={'Men'}
+        title={'Tooth Powder'}
         text={'SHOP NOW'}
         link={'/shop'}
       />
       <ProductCollection
         image={'/collections/collection2.png'}
-        title={'Women'}
+        title={'Hair oil'}
         text={'SHOP NOW'}
         link={'/shop'}
       />
       <ProductCollection
-        image={'/collections/collection3.png'}
-        title={'Accessories'}
+        image={'/collections/collection1.png'}
+        title={'Face Pack'}
         text={'SHOP NOW'}
         link={'/shop'}
       />
       <ProductCollection
-        image={'/collections/collection4.png'}
-        title={'Simple Cotton'}
+        image={'/collections/collection2.png'}
+        title={'Herbal Soap'}
         text={'SHOP NOW'}
         link={'/shop'}
       />
