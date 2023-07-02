1a56a644c8c8f3e77e3896ba554225fa08590c3f
Supriya K Hemanth <supriya1890@gmail.com>
2023-07-02 16:54:44 +0000

    updated product image

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import AttributeGrid from '../components/AttributeGrid';
 import Container from '../components/Container';
 import Hero from '../components/Hero';
 import BlogPreviewGrid from '../components/BlogPreviewGrid';
-import Highlight from '../components/Highlight';
+// import Highlight from '../components/Highlight';
 import Layout from '../components/Layout/Layout';
 import ProductCollectionGrid from '../components/ProductCollectionGrid';
 import ProductCardGrid from '../components/ProductCardGrid';
